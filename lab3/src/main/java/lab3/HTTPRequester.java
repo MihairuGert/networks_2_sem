3eaae5d3ef56e0731544ca5b2c334a6316b70dcb
MihairuGert <vertygert4949@gmail.com>
2025-09-13 11:22:45 +0700

    lab3 added json

--- lab3/src/main/java/lab3/HTTPRequester.java
+++ lab3/src/main/java/lab3/HTTPRequester.java
@@ -1,7 +1,5 @@
 package lab3;
 
-import netscape.javascript.JSObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -32,6 +30,7 @@ public class HTTPRequester {
 
             String response = responseBuilder.toString();
             System.out.println(response);
+            //JSONObject
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
