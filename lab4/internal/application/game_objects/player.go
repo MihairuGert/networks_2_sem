e422917c0463fac09641a10b22d539e7d42efce8
mikhail <vertygert4949@gmail.com>
2025-10-08 15:30:00 +0700

    fixed fucking snake

--- lab4/internal/application/game_objects/player.go
+++ lab4/internal/application/game_objects/player.go
@@ -25,14 +25,22 @@ func NewPlayer(x, y int32) *Player {
 	player.velocity = 1
 
 	points := make([]*domain.GameState_Coord, 0)
-	lcoords := make([]int32, 4)
+	lcoords := make([]int32, 8)
 	lcoords[0] = x
 	lcoords[1] = y
 	lcoords[2] = -1
 	lcoords[3] = 0
 
+	lcoords[4] = -1
+	lcoords[5] = 0
+
+	lcoords[6] = -1
+	lcoords[7] = 0
+
 	points = append(points, &domain.GameState_Coord{X: &lcoords[0], Y: &lcoords[1]})
 	points = append(points, &domain.GameState_Coord{X: &lcoords[2], Y: &lcoords[3]})
+	points = append(points, &domain.GameState_Coord{X: &lcoords[4], Y: &lcoords[5]})
+	points = append(points, &domain.GameState_Coord{X: &lcoords[6], Y: &lcoords[7]})
 
 	player.snake = domain.GameState_Snake{Points: points}
 	curDir := domain.Direction_RIGHT
@@ -42,6 +50,11 @@ func NewPlayer(x, y int32) *Player {
 }
 
 func (player *Player) Move(direction domain.Direction) {
+	for i := len(player.snake.Points) - 1; i > 1; i-- {
+		*player.snake.Points[i].X = *player.snake.Points[i-1].X
+		*player.snake.Points[i].Y = *player.snake.Points[i-1].Y
+	}
+
 	*player.snake.HeadDirection = direction
 	switch *player.snake.HeadDirection {
 	case domain.Direction_UP:
